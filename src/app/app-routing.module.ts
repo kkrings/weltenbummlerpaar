a78873a9fe54c07edb96f1e9c601bbc1bbad1d53
Kai Krings <kai.krings@posteo.de>
2020-02-12 17:19:40 +0100

    Add a new component for showing the full diary entry; link this component via routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { EntriesComponent } from './entries/entries.component';
+import { EntryComponent } from './entry/entry.component';
 
 
 const routes: Routes = [
-  {path: '', component: EntriesComponent}
+  {path: '', component: EntriesComponent},
+  {path: 'entries/:entryId', component: EntryComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntriesComponent } from './entries/entries.component';
+import { EntryComponent } from './entry/entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntriesComponent
+    EntriesComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
