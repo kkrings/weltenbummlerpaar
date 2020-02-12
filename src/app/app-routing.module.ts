890a1cfdcd6a35db719093385c72260e0326d712
Kai Krings <kai.krings@posteo.de>
2020-02-12 15:01:24 +0100

    Use routing for showing the list of diary entries at '/'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EntriesComponent } from './entries/entries.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: EntriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
