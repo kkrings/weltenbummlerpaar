fbe7055de45b2d3eb38080ce28d6890e2bd89427
Kai Krings <kai.krings@posteo.de>
2020-02-24 15:22:45 -0600

    Add method to image carousel component that returns an image's URL.

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -1,19 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Image } from '../shared/image.model';
+import { ImageService } from '../shared/image.service';
 
+
+/**
+ * Show diary entry's images in a Bootstrap carousel.
+ */
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.scss']
 })
 export class ImageCarouselComponent implements OnInit {
-
+  /**
+   * List of images to show in image carousel
+   */
   @Input() images: Image[];
 
+  /**
+   * Construct image carousel component.
+   */
   constructor() { }
 
+  /**
+   * Initialize image carousel component.
+   */
   ngOnInit() {
   }
 
+  /**
+   * Get image's URL.
+   *
+   * @param image
+   *   Image
+   *
+   * @returns
+   *   Image's URL
+   */
+  getImageUrl(image: Image): string {
+    return ImageService.getImageUrl(image);
+  }
 }
