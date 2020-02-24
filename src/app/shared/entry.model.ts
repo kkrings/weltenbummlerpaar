7b11293ec1305c00d5f94ec147fc3cb1cad2341a
Kai Krings <kai.krings@posteo.de>
2020-02-24 15:16:49 -0600

    Add updateAt attribute to diary entry model.

--- src/app/shared/entry.model.ts
+++ src/app/shared/entry.model.ts
@@ -4,10 +4,38 @@ import { Image } from './image.model';
  * Diary entry model
  */
 export interface DiaryEntry {
+  /**
+   * Diary entry's ID
+   */
   _id: string;
+
+  /**
+   * Diary entry's title
+   */
   title: string;
+
+  /**
+   * Diary entry's body
+   */
   body: string;
+
+  /**
+   * Country name the diary entry refers to
+   */
   country: string;
+
+  /**
+   * List of image objects
+   */
   images: Image[];
+
+  /**
+   * Diary entry's creation date and time
+   */
   createdAt: string;
+
+  /**
+   * Date and time when the diary entry was last updated.
+   */
+  updatedAt: string;
 }
