16a393982714eac4e84d67f7fb2203af1be1724d
Kai Krings <kai.krings@posteo.de>
2020-02-24 15:17:57 -0600

    Add updatedAt attribute to image model; remove path attribute, because the image's URL can be constructed via its ID.

--- src/app/shared/image.model.ts
+++ src/app/shared/image.model.ts
@@ -2,7 +2,23 @@
  * Image model
  */
 export interface Image {
+  /**
+   * Image's ID
+   */
   _id: string;
-  path: string;
+
+  /**
+   * Image's description
+   */
   description: string;
+
+  /**
+   * Image's creation date and time
+   */
+  createdAt: string;
+
+  /**
+   * Date and time when image was last updated.
+   */
+  updatedAt: string;
 }
