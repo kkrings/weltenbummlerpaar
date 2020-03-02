6214cae78ca9222fb061092aaa9b24088c4a97ce
Kai Krings <kai.krings@posteo.de>
2020-03-02 14:44:35 -0600

    Comment out all unit tests except for the image service.

--- src/app/entry-grid/entry-grid.component.spec.ts
+++ src/app/entry-grid/entry-grid.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { EntryGridComponent } from './entry-grid.component';
-
-describe('EntryGridComponent', () => {
-  let component: EntryGridComponent;
-  let fixture: ComponentFixture<EntryGridComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EntryGridComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EntryGridComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { EntryGridComponent } from './entry-grid.component';
+//
+// describe('EntryGridComponent', () => {
+//   let component: EntryGridComponent;
+//   let fixture: ComponentFixture<EntryGridComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ EntryGridComponent ]
+//     })
+//     .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(EntryGridComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/shared/image.service.spec.ts
+++ src/app/shared/image.service.spec.ts
@@ -1,12 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+/**
+ * Unit tests for image service
+ * @packageDocumentation
+ */
 
 import { ImageService } from './image.service';
+import { Image } from './image.model';
+import { environment } from '../../environments/environment';
+
 
 describe('ImageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  it('#getImageUrl should return image\'s URL', () => {
+    const image: Image = {
+      _id: '5e558e05834fb6e13158eb74',
+      description: 'This is a mock image for testing.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
 
-  it('should be created', () => {
-    const service: ImageService = TestBed.get(ImageService);
-    expect(service).toBeTruthy();
+    expect(ImageService.getImageUrl(image))
+        .toBe(`${environment.baseurl}/images/${image._id}.jpg`);
   });
 });
