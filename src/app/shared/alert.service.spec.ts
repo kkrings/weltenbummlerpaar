ec9ea5ce5bf449e569d6c92ce7cdbf2720033141
Kai Krings <kai.krings@posteo.de>
2020-03-02 18:56:43 -0600

    Add unit tests for alert service.

--- src/app/shared/alert.service.spec.ts
+++ src/app/shared/alert.service.spec.ts
@@ -1,12 +1,41 @@
-// import { TestBed } from '@angular/core/testing';
-//
-// import { AlertService } from './alert.service';
-//
-// describe('AlertService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-//
-//   it('should be created', () => {
-//     const service: AlertService = TestBed.get(AlertService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+/**
+ * Unit tests for alert service
+ * @packageDocumentation
+ */
+
+import { TestBed } from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { AlertService } from './alert.service';
+
+
+describe('AlertService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('#handleError should return user-friendly client-side error', () => {
+    const service: AlertService = TestBed.get(AlertService);
+
+    const error = new HttpErrorResponse({
+        error: new ErrorEvent('MockClientSideErrorEvent', {
+          message: 'This is a mock client-side error event.'
+        })
+    });
+
+    service.handleError(error).subscribe(
+      fail, (message: string) => expect(message)
+          .toContain('Ich kann nicht mit dem Backendserver reden.'));
+  });
+
+  it('#handleError should return user-friendly server-side error', () => {
+    const service: AlertService = TestBed.get(AlertService);
+
+    const error = new HttpErrorResponse({
+        error: new Error('This is a mock server-side error event.'),
+        status: 500
+    });
+
+    service.handleError(error).subscribe(
+      fail, (message: string) => expect(message)
+          .toContain('Der Backendserver mag nicht mit mir reden.'));
+  });
+});
