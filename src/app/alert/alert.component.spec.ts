d522610d53716b2a34ac12f05796933504176f13
Kai Krings <kai.krings@posteo.de>
2020-03-03 20:01:39 -0600

    Add unit tests for alert component.

--- src/app/alert/alert.component.spec.ts
+++ src/app/alert/alert.component.spec.ts
@@ -1,25 +1,47 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { AlertComponent } from './alert.component';
-//
-// describe('AlertComponent', () => {
-//   let component: AlertComponent;
-//   let fixture: ComponentFixture<AlertComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AlertComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//  beforeEach(() => {
-//     fixture = TestBed.createComponent(AlertComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+/**
+ * Unit tests for alert component
+ * @packageDocumentation
+ */
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AlertModule } from 'ngx-bootstrap/alert';
+
+import { AlertComponent } from './alert.component';
+
+
+describe('AlertComponent', () => {
+  let component: AlertComponent;
+  let fixture: ComponentFixture<AlertComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AlertModule.forRoot()
+      ],
+      declarations: [
+        AlertComponent
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be hidden if alert message is undefined', () => {
+    const element = fixture.debugElement.nativeElement;
+    expect(element.querySelector('div')).toBeNull();
+  });
+
+  it('should render alert message', () => {
+    const testMessage = 'This is a test alert message.';
+
+    component.alertMessage = testMessage;
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.nativeElement;
+    expect(element.querySelector('alert').textContent).toMatch(testMessage);
+  });
+});
