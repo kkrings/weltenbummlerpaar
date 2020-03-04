8ed92ce8b64e24ea44a498bc89799b112dc06cd8
Kai Krings <kai.krings@posteo.de>
2020-03-04 14:51:50 -0600

    Add unit tests for spinner component.

--- src/app/spinner/spinner.component.spec.ts
+++ src/app/spinner/spinner.component.spec.ts
@@ -1,25 +1,43 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { SpinnerComponent } from './spinner.component';
-//
-// describe('SpinnerComponent', () => {
-//   let component: SpinnerComponent;
-//   let fixture: ComponentFixture<SpinnerComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SpinnerComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SpinnerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+/**
+ * Unit tests for spinner component
+ * @packageDocumentation
+ */
+
+import { async, ComponentFixture, TestBed, } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { SpinnerComponent } from './spinner.component';
+
+
+describe('SpinnerComponent', () => {
+  let component: SpinnerComponent;
+  let fixture: ComponentFixture<SpinnerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SpinnerComponent
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpinnerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be hidden by default', () => {
+    expect(component.showSpinner).toBeFalsy();
+    const element = fixture.debugElement.query(By.css('.spinner-grow'));
+    expect(element).toBeNull();
+  });
+
+  it('should be rendered', () => {
+    component.showSpinner = true;
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('.spinner-grow'));
+    expect(element).toBeDefined();
+    expect(element.nativeElement.textContent).toContain('Loading');
+  });
+});
