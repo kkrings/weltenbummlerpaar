b30c366ff7cbe9010c58720726e0a95f8eb26840
Kai Krings <kai.krings@posteo.de>
2020-04-13 15:01:49 +0200

    Add more unit tests for image service.

--- src/app/image/image.service.spec.ts
+++ src/app/image/image.service.spec.ts
@@ -10,6 +10,8 @@ import {
   HttpTestingController
 } from '@angular/common/http/testing';
 
+import * as Jimp from 'jimp';
+
 import { ImageService } from './image.service';
 import { Image } from './image.model';
 import { HttpAlertService } from '../shared/http-alert.service';
@@ -45,6 +47,137 @@ describe('ImageService', () => {
 
     expect(ImageService.getImageUrl(image))
         .toBe(`${environment.baseurl}/images/${image._id}.jpg`);
+  });
+
+  it('#uploadImage should return image', async () => {
+    const testEntryId = '0';
+
+    const buffer = await (new Jimp(16, 16)).getBufferAsync(Jimp.MIME_JPEG);
+
+    const testImage: Image = {
+      file: new File([buffer], 'testImage.jpg'),
+      _id: '0',
+      description: 'This is a test image.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
+
+    service.uploadImage(testEntryId, testImage).subscribe(
+        (image: Image) => expect(image).toEqual(testImage),
+        fail);
+
+    const testRequest = httpTestingController.expectOne(
+        `${environment.baseurl}/db/entries/${testEntryId}/images`);
+
+    expect(testRequest.request.method).toMatch('POST');
+
+    testRequest.flush(testImage);
+  });
+
+  it('#uploadImage without file should throw', () => {
+    const testEntryId = '0';
+
+    const testImage: Image = {
+      _id: '0',
+      description: 'This is a test image.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
+
+    service.uploadImage(testEntryId, testImage).subscribe(
+        fail, (error: string) => expect(error).toBeDefined());
+  });
+
+  it('#uploadImage should return alert message', async () => {
+    const testEntryId = '0';
+
+    const buffer = await (new Jimp(16, 16)).getBufferAsync(Jimp.MIME_JPEG);
+
+    const testImage: Image = {
+      file: new File([buffer], 'testImage.jpg'),
+      _id: '0',
+      description: 'This is a test image.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
+
+    service.uploadImage(testEntryId, testImage).subscribe(
+        fail, (message: string) => expect(message).toBeDefined());
+
+    const testRequest = httpTestingController.expectOne(
+        `${environment.baseurl}/db/entries/${testEntryId}/images`);
+
+    expect(testRequest.request.method).toMatch('POST');
+
+    testRequest.flush('mock HTTP error response', {
+      status: 500,
+      statusText: 'Mock error on back-end server'
+    });
+  });
+
+  it('#updateImage should return image', async () => {
+    const buffer = await (new Jimp(16, 16)).getBufferAsync(Jimp.MIME_JPEG);
+
+    const testImage: Image = {
+      file: new File([buffer], 'testImage.jpg'),
+      _id: '0',
+      description: 'This is a test image.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
+
+    service.updateImage(testImage).subscribe(
+        (image: Image) => expect(image).toEqual(testImage),
+        fail);
+
+    const testRequest = httpTestingController.expectOne(
+        `${environment.baseurl}/db/images/${testImage._id}`);
+
+    expect(testRequest.request.method).toMatch('PUT');
+
+    testRequest.flush(testImage);
+  });
+
+  it('#updateImage without file should return image', async () => {
+    const testImage: Image = {
+      _id: '0',
+      description: 'This is a test image.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
+
+    service.updateImage(testImage).subscribe(
+        (image: Image) => expect(image).toEqual(testImage),
+        fail);
+
+    const testRequest = httpTestingController.expectOne(
+        `${environment.baseurl}/db/images/${testImage._id}`);
+
+    expect(testRequest.request.method).toMatch('PUT');
+
+    testRequest.flush(testImage);
+  });
+
+  it('#updateImage should return alert message', async () => {
+    const testImage: Image = {
+      _id: '0',
+      description: 'This is a test image.',
+      createdAt: (new Date()).toISOString(),
+      updatedAt: (new Date()).toISOString()
+    };
+
+    service.updateImage(testImage).subscribe(
+        fail, (message: string) => expect(message).toBeDefined());
+
+    const testRequest = httpTestingController.expectOne(
+        `${environment.baseurl}/db/images/${testImage._id}`);
+
+    expect(testRequest.request.method).toMatch('PUT');
+
+    testRequest.flush('mock HTTP error response', {
+      status: 500,
+      statusText: 'Mock error on back-end server'
+    });
   });
 
   it('#delteImage should return image', () => {
