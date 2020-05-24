7c7df35a48582bfdafe232217cd7d2dc8e55d810
Kai Krings <kai.krings@posteo.de>
2020-05-24 12:56:44 +0200

    Add unit tests for image carousel component.

--- src/app/image/image-carousel/image-carousel.component.spec.ts
+++ src/app/image/image-carousel/image-carousel.component.spec.ts
@@ -1,25 +1,69 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { ImageCarouselComponent } from './image-carousel.component';
-//
-// describe('ImageCarouselComponent', () => {
-//   let component: ImageCarouselComponent;
-//   let fixture: ComponentFixture<ImageCarouselComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ImageCarouselComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ImageCarouselComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//    expect(component).toBeTruthy();
-//   });
-// });
+/**
+ * Unit tests for image carousel component
+ * @packageDocumentation
+ */
+
+import { Directive, Input } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ImageCarouselComponent } from './image-carousel.component';
+import { DIARY_ENTRIES } from '../../diary-entry/diary-entries';
+
+
+/**
+ * Mock image directive
+ */
+@Directive({
+  selector: '[appImage]'
+})
+class MockImageDirective {
+  /**
+   * Mock image
+   */
+  @Input('appImage') mockImage = null;
+}
+
+
+describe('ImageCarouselComponent', () => {
+  let component: ImageCarouselComponent;
+  let fixture: ComponentFixture<ImageCarouselComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        NgbCarouselModule
+      ],
+      declarations: [
+        ImageCarouselComponent,
+        MockImageDirective
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageCarouselComponent);
+    component = fixture.componentInstance;
+    component.imageList = [...DIARY_ENTRIES[0].images];
+    fixture.detectChanges();
+  });
+
+  it('should render images', () => {
+    const slides = fixture.debugElement.queryAll(By.css('.carousel-item'));
+    expect(slides.length).toEqual(component.imageList.length);
+  });
+
+  it('should render image\'s index', () => {
+    const index = fixture.debugElement.query(By.css('small.text-muted'));
+
+    expect(index.nativeElement.textContent).toMatch(
+      `1/${component.imageList.length}`);
+  });
+
+  it('should render image\'s description', () => {
+    const caption = fixture.debugElement.query(By.css('.figure-caption'));
+
+    expect(caption.nativeElement.textContent).toEqual(
+        component.imageList[0].description); });
+});
