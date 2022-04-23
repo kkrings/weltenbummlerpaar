296c4ab8c90a2e1eba0338c430160ad2dce4bc78
Kai Krings <kai.krings@posteo.de>
2022-04-23 21:41:27 +0000

    Provide unit tests for DateRangePipe.

--- src/app/date-range/date-range.pipe.spec.ts
+++ src/app/date-range/date-range.pipe.spec.ts
@@ -1,8 +1,44 @@
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
 import { DateRangePipe } from './date-range.pipe';
 
+registerLocaleData(localeDe);
+
 describe('DateRangePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateRangePipe('de');
-    expect(pipe).toBeTruthy();
+  let pipe: DateRangePipe;
+
+  beforeEach(() => {
+    pipe = new DateRangePipe('de');
+  });
+
+  describe('#dateMin equals #dateMax', () => {
+    let dateRange: string;
+
+    beforeEach(() => {
+      dateRange = pipe.transform({
+        dateMin: '2020-02-14',
+        dateMax: '2020-02-14',
+      });
+    });
+
+    it('#dateRange should have been transformed correctly', () => {
+      expect(dateRange).toEqual('14.02.2020');
+    });
+  });
+
+  describe('#dateMin before #dateMax', () => {
+    let dateRange: string;
+
+    beforeEach(() => {
+      dateRange = pipe.transform({
+        dateMin: '2020-02-14',
+        dateMax: '2020-02-15',
+      });
+    });
+
+    it('#dateRange should have been transformed correctly', () => {
+      expect(dateRange).toEqual('14.02.2020 bis 15.02.2020');
+    });
   });
 });
