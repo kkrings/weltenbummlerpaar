44f3962906cd5c16fd34b9546d4e8600be35b1e8
Kai Krings <kai.krings@posteo.de>
2022-05-06 22:50:35 +0000

    Add doc strings to date range select day component.

--- src/app/date-range/date-range-select-day/date-range-select-day.component.spec.ts
+++ src/app/date-range/date-range-select-day/date-range-select-day.component.spec.ts
@@ -1,3 +1,8 @@
+/**
+ * Unit tests for date range select day component
+ * @packageDocumentation
+ */
+
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
--- src/app/date-range/date-range-select-day/date-range-select-day.component.ts
+++ src/app/date-range/date-range-select-day/date-range-select-day.component.ts
@@ -1,26 +1,53 @@
+/**
+ * Date range select day component
+ * @packageDocumentation
+ */
+
 import { Component, Input } from '@angular/core';
 import { NgbDate, NgbDatepickerI18n } from '@ng-bootstrap/ng-bootstrap';
 
 import { isOpen, isRange } from '../date-range-utils';
 import { NgbDateRange } from '../ngb-date-range.model';
 
+/**
+ * Date range select day component
+ *
+ * This component makes sure that the date range, which is selected by the user,
+ * is properly highlighted in the calendar. It is used within the calendar's
+ * template for rendering days.
+ */
 @Component({
   selector: 'app-date-range-select-day',
   templateUrl: './date-range-select-day.component.html',
   styleUrls: ['./date-range-select-day.component.scss'],
 })
 export class DateRangeSelectDayComponent {
+  /**
+   * Holds a reference to the current date range.
+   */
   @Input()
   dateRange: NgbDateRange = {
     dateMin: null,
     dateMax: null,
   };
 
+  /**
+   * Holds a reference to the date that this component is rendering.
+   */
   @Input()
   date = new NgbDate(2020, 2, 14);
 
+  /**
+   * Initialize a new instance.
+   *
+   * @param i18n
+   *   Service for extracting and formatting the `date`'s day part.
+   */
   constructor(private readonly i18n: NgbDatepickerI18n) {}
 
+  /**
+   * Specifies if the `date` is equal to the date range's start date.
+   */
   get dateIsDateMin(): boolean {
     return (
       (isOpen(this.dateRange) || isRange(this.dateRange)) &&
@@ -28,10 +55,16 @@ export class DateRangeSelectDayComponent {
     );
   }
 
+  /**
+   * Specifies if the `date` is equal to the date range's end date.
+   */
   get dateIsDateMax(): boolean {
     return isRange(this.dateRange) && this.date.equals(this.dateRange.dateMax);
   }
 
+  /**
+   * Specifies if the `date` is in between the date range's start and end dates.
+   */
   get dateIsInRange(): boolean {
     return (
       isRange(this.dateRange) &&
@@ -40,6 +73,15 @@ export class DateRangeSelectDayComponent {
     );
   }
 
+  /**
+   * Extract and format the day part from the given date.
+   *
+   * @param date
+   *   The date
+   *
+   * @returns
+   *   The formatted day part
+   */
   getDay(date: NgbDate): string {
     return this.i18n.getDayNumerals(date);
   }
