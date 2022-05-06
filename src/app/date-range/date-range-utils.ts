e033fc6a30ecd6f748ca68d3757fe0b7ef2fbea4
Kai Krings <kai.krings@posteo.de>
2022-05-06 03:42:37 +0000

    Add more doc strings to the date range source files.

--- src/app/date-range/date-range-utils.ts
+++ src/app/date-range/date-range-utils.ts
@@ -1,15 +1,54 @@
+/**
+ * Date range utility functions
+ * @packageDocumentation
+ */
+
 import { DateRange } from './date-range.model';
 import { NgbDateRange } from './ngb-date-range.model';
 
+/**
+ * Check if the given date range is empty.
+ *
+ * A date range is empty if neither start nor end date are specified; meaning,
+ * they are both empty strings.
+ *
+ * @param dateRange
+ *   The date range
+ *
+ * @returns
+ *   If `true`, the given date range is empty.
+ */
 export function isEmpty(dateRange: DateRange): boolean {
   return dateRange.dateMin === '' && dateRange.dateMax === '';
 }
 
+/**
+ * Check if the given parsed date range is open.
+ *
+ * A date range is open if only its start date is specified.
+ *
+ * @param dateRange
+ *   The date range
+ *
+ * @returns
+ *   If `true`, the given parsed date range is open.
+ */
 export function isOpen(dateRange: NgbDateRange): boolean {
   const { dateMin, dateMax } = dateRange;
   return dateMin !== null && dateMax === null;
 }
 
+/**
+ * Check if the given parsed date range is valid.
+ *
+ * A date range is valid if its start date is before or equal to its end date.
+ *
+ * @param dateRange
+ *   The date range
+ *
+ * @returns
+ *   If `true`, the given parsed date range is valid.
+ */
 export function isRange(dateRange: NgbDateRange): boolean {
   const { dateMin, dateMax } = dateRange;
   return dateMin?.before(dateMax) || dateMin?.equals(dateMax) || false;
--- src/app/date-range/date-range-validator.ts
+++ src/app/date-range/date-range-validator.ts
@@ -1,26 +1,80 @@
+/**
+ * Date range validator
+ * @packageDocumentation
+ */
+
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 import { isRange } from './date-range-utils';
 import { DateRange } from './date-range.model';
 import { DateRangeService } from './date-range.service';
 
+/**
+ * Date range validator
+ *
+ * This class validates date ranges. A date range is valid if it is
+ * either `null` or if its start date is before or equal to its end date.
+ */
 export class DateRangeValidator {
+  /**
+   * Initialize a new instance.
+   *
+   * @param dateRangeService
+   *   Service for parsing date ranges
+   */
   constructor(private readonly dateRangeService: DateRangeService) {}
 
+  /**
+   * Apply the date range validation to the given form control.
+   *
+   * @param control
+   *   The form control
+   *
+   * @returns
+   *   If `null`, the date range validation has passed; otherwise, an invalid
+   *   date range error is returned.
+   */
   isValid(control: AbstractControl): ValidationErrors | null {
     return this.isNullOrRange(control.value)
       ? null
       : { invalidDateRange: true };
   }
 
+  /**
+   * Check if the given date range is `null` or valid.
+   *
+   * @param dateRange
+   *   The date range
+   *
+   * @returns
+   *   If `true`, the given date range is either `null` or valid.
+   */
   private isNullOrRange(dateRange: DateRange | null): boolean {
     return dateRange === null || this.isRange(dateRange);
   }
 
+  /**
+   * Check if the given date range is valid.
+   *
+   * @param dateRange
+   *   The date range
+   *
+   * @returns
+   *   If `true`, the given date range is valid.
+   */
   private isRange(dateRange: DateRange): boolean {
     return isRange(this.dateRangeService.parseDateRange(dateRange));
   }
 
+  /**
+   * Date range validation function
+   *
+   * @param dateRangeService
+   *   Service for parsing date ranges
+   *
+   * @returns
+   *   Date range validation function
+   */
   static isValid(dateRangeService: DateRangeService): ValidatorFn {
     const validator = new this(dateRangeService);
     return (control) => validator.isValid(control);
